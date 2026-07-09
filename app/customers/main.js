ecd2fa4e281f2155295b561b1a8ea10cf2a1e133
Thinhph9 <thinhphhv@fullstack.edu.vn>
2026-07-09 23:22:42 +0700

    fix

--- app/customers/main.js
+++ app/customers/main.js
@@ -1,139 +1,139 @@
-import {
-    getCustomers,
-    addCustomer,
-    updateCustomer,
-    deleteCustomer
-}
-    from "./services/customer.service.js";
+// import {
+//     getCustomers,
+//     addCustomer,
+//     updateCustomer,
+//     deleteCustomer
+// }
+//     from "./services/customer.service.js";
 
-import {
-    renderCustomers
-}
-    from "./componets/customer.render.js";
+// import {
+//     renderCustomers
+// }
+//     from "./componets/customer.render.js";
 
-let customers = [];
-let currentId = null;
+// let customers = [];
+// let currentId = null;
 
-const modal = document.getElementById("modal");
-const btnSaveCustomer =
-    document.getElementById("btnSaveCustomer");
-const nameInput =
-    document.getElementById("name");
-const emailInput =
-    document.getElementById("email");
-const phoneInput =
-    document.getElementById("phone");
-const tierInput =
-    document.getElementById("tier");
-btnSaveCustomer.addEventListener(
-    "click",
-    saveCustomer
-);
+// const modal = document.getElementById("modal");
+// const btnSaveCustomer =
+//     document.getElementById("btnSaveCustomer");
+// const nameInput =
+//     document.getElementById("name");
+// const emailInput =
+//     document.getElementById("email");
+// const phoneInput =
+//     document.getElementById("phone");
+// const tierInput =
+//     document.getElementById("tier");
+// btnSaveCustomer.addEventListener(
+//     "click",
+//     saveCustomer
+// );
 
-const btnCancel =
-    document.getElementById(
-        "btnCancel"
-    );
+// const btnCancel =
+//     document.getElementById(
+//         "btnCancel"
+//     );
 
-const btnAddCustomer =
-    document.getElementById(
-        "btnAddCustomer"
-    );
+// const btnAddCustomer =
+//     document.getElementById(
+//         "btnAddCustomer"
+//     );
 
-btnAddCustomer.addEventListener(
-    "click",
-    () => {
-        currentId = null;
-        nameInput.value = "";
-        emailInput.value = "";
-        phoneInput.value = "";
-        tierInput.value = "gold";
+// btnAddCustomer.addEventListener(
+//     "click",
+//     () => {
+//         currentId = null;
+//         nameInput.value = "";
+//         emailInput.value = "";
+//         phoneInput.value = "";
+//         tierInput.value = "gold";
 
-        modal.style.display = "flex";
-    }
-);
-btnCancel.addEventListener(
-    "click",
-    closeModal
-);
-
-
-async function loadCustomers() {
-    customers = await getCustomers();
-    renderCustomers(customers);
-}
-loadCustomers();
-
-async function saveCustomer() {
-    if (currentId) {
-        await editCustomerData();
-    }
-    else {
-        await createCustomer();
-    }
-}
+//         modal.style.display = "flex";
+//     }
+// );
+// btnCancel.addEventListener(
+//     "click",
+//     closeModal
+// );
 
 
-async function createCustomer() {
-    const customer = {
-        name: nameInput.value,
-        email: emailInput.value,
-        phone: phoneInput.value,
-        tier: tierInput.value
-    };
+// async function loadCustomers() {
+//     customers = await getCustomers();
+//     renderCustomers(customers);
+// }
+// loadCustomers();
+
+// async function saveCustomer() {
+//     if (currentId) {
+//         await editCustomerData();
+//     }
+//     else {
+//         await createCustomer();
+//     }
+// }
 
 
-    await addCustomer(customer);
-    await loadCustomers();
-    closeModal();
-
-}
-
-
-async function editCustomerData() {
-    const customer = {
-        name: nameInput.value,
-        email: emailInput.value,
-        phone: phoneInput.value,
-        tier: tierInput.value
-
-    };
-
-    await updateCustomer(currentId, customer);
-    currentId = null;
-    await loadCustomers();
-    closeModal();
-}
+// async function createCustomer() {
+//     const customer = {
+//         name: nameInput.value,
+//         email: emailInput.value,
+//         phone: phoneInput.value,
+//         tier: tierInput.value
+//     };
 
 
-window.editCustomer = function (id) {
+//     await addCustomer(customer);
+//     await loadCustomers();
+//     closeModal();
 
-    currentId = id;
+// }
 
-    const customer = customers.find(c => c.id === id);
-    if (!customer) return;
-    nameInput.value = customer.name;
-    emailInput.value = customer.email;
-    phoneInput.value = customer.phone;
-    tierInput.value = customer.tier;
-    modal.style.display = "flex";
 
-};
+// async function editCustomerData() {
+//     const customer = {
+//         name: nameInput.value,
+//         email: emailInput.value,
+//         phone: phoneInput.value,
+//         tier: tierInput.value
 
-window.removeCustomer = async function (id) {
-    const ok = confirm("Bạn có chắc muốn xóa?");
-    if (!ok) return;
-    await deleteCustomer(id);
-    await loadCustomers();
-};
-function closeModal() {
-    modal.style.display = "none";
-}
-import { headers, renderTable, get } from './ulits'
+//     };
 
-const init = async () => {
-    const customers = await get('')
+//     await updateCustomer(currentId, customer);
+//     currentId = null;
+//     await loadCustomers();
+//     closeModal();
+// }
 
-    await renderTable(headers, customers)
-}
-init()
+
+// window.editCustomer = function (id) {
+
+//     currentId = id;
+
+//     const customer = customers.find(c => c.id === id);
+//     if (!customer) return;
+//     nameInput.value = customer.name;
+//     emailInput.value = customer.email;
+//     phoneInput.value = customer.phone;
+//     tierInput.value = customer.tier;
+//     modal.style.display = "flex";
+
+// };
+
+// window.removeCustomer = async function (id) {
+//     const ok = confirm("Bạn có chắc muốn xóa?");
+//     if (!ok) return;
+//     await deleteCustomer(id);
+//     await loadCustomers();
+// };
+// function closeModal() {
+//     modal.style.display = "none";
+// }
+// import { headers, renderTable, get } from './ulits'
+
+// const init = async () => {
+//     const customers = await get('')
+
+//     await renderTable(headers, customers)
+// }
+// init()
--- app/customers/services/customer.service.js
+++ app/customers/services/customer.service.js
@@ -1,78 +1,78 @@
-import {API_URL,TOKEN} from "../config/api.js";
+// import {API_URL,TOKEN} from "../config/api.js";
 
-const headers = {
+// const headers = {
 
-    "Content-Type":"application/json",
+//     "Content-Type":"application/json",
 
-    Authorization:`Bearer ${TOKEN}`
+//     Authorization:`Bearer ${TOKEN}`
 
-};
+// };
 
-export async function getCustomers(){
-    const response = await fetch(
-        API_URL,
-        {
-            headers
-        }
-    );
-    return await response.json();
+// export async function getCustomers(){
+//     const response = await fetch(
+//         API_URL,
+//         {
+//             headers
+//         }
+//     );
+//     return await response.json();
 
-}
-
-
-export async function addCustomer(customer){
-
-    const response = await fetch(
-
-        API_URL,
-
-        {
-
-            method:"POST",
-
-            headers,
-
-            body:JSON.stringify(customer)
-
-        }
-
-    );
-
-    return await response.json();
-
-}
+// }
 
 
+// export async function addCustomer(customer){
 
-export async function updateCustomer(id, customer){
+//     const response = await fetch(
 
-    const response = await fetch(
-        `${API_URL}/${id}`,
-        {
-            method:"PUT",
-            headers,
-            body:JSON.stringify(customer)
-        }
-    );
-    return await response.json();
+//         API_URL,
 
-}
+//         {
+
+//             method:"POST",
+
+//             headers,
+
+//             body:JSON.stringify(customer)
+
+//         }
+
+//     );
+
+//     return await response.json();
+
+// }
 
 
-export async function deleteCustomer(id){
 
-    console.log("Delete ID:",id);
-    const response = await fetch(
-        `${API_URL}/${id}`,
-        {
-            method:"DELETE",
-            headers:{
-                Authorization:`Bearer ${TOKEN}`
-            }
-        }
-    );
-    console.log(response.status);
+// export async function updateCustomer(id, customer){
 
-}
+//     const response = await fetch(
+//         `${API_URL}/${id}`,
+//         {
+//             method:"PUT",
+//             headers,
+//             body:JSON.stringify(customer)
+//         }
+//     );
+//     return await response.json();
+
+// }
+
+
+// export async function deleteCustomer(id){
+
+//     console.log("Delete ID:",id);
+//     const response = await fetch(
+//         `${API_URL}/${id}`,
+//         {
+//             method:"DELETE",
+//             headers:{
+//                 Authorization:`Bearer ${TOKEN}`
+//             }
+//         }
+//     );
+//     console.log(response.status);
+
+// }
 
 
